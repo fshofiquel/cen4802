e75b317f345d9cb1123b1df8c31600f349c5ae90
fshofiquel <45765393+fshofiquel@users.noreply.github.com>
2026-05-16 20:51:24 -0400

    Added Fib

--- src/DriverFunction.java
+++ src/DriverFunction.java
@@ -1,2 +1,10 @@
+import math.Fibonacci;
+
 public class DriverFunction {
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+
+        System.out.println(fibonacci.nthFibonacci(10));
+    }
 }
--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -1,4 +1,14 @@
 package math;
 
 public class Fibonacci {
+    public static int nthFibonacci(int n) {
+
+        // base case
+        if (n <= 1) {
+            return n;
+        }
+        // sum of the two preceding
+        // Fibonacci numbers
+        return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+    }
 }
